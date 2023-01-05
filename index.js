81fc1edc490426de7c9dad43a827b7e35c025bf8
Karthik M <53159415+karthik-m96@users.noreply.github.com>
2023-01-05 10:55:04 +0530

    json

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-let json = [{
-    "id" : "1", 
-    "name"   : "Karthik",
-    "number" : "9551565454",
-    "mail": "karthik@gmail.com"
-},
-{
-    "id" : "2", 
-    "name"   : "balaji",
-    "number" : "24654654564",
-    "mail": "balaji@gmail.com"
-},
-{
-    "id" : "3", 
-    "name"   : "Dinesh",
-    "number" : "42578546456",
-    "mail": "dinesh@gmail.com"
-}];
+let json = [
+  {
+    id: "1",
+    name: "Karthik",
+    number: "9551565454",
+    mail: "karthik@gmail.com",
+  },
+  {
+    id: "2",
+    name: "balaji",
+    number: "24654654564",
+    mail: "balaji@gmail.com",
+  },
+  {
+    id: "3",
+    name: "Dinesh",
+    number: "42578546456",
+    mail: "dinesh@gmail.com",
+  },
+];
 
-for(var i = 0; i < json.length; i++) {
-    var obj = json[i];
+for (let i = 0; i < json.length; i++) {
+  let obj = json[i];
 
-    console.log(obj.id);
-    console.log(obj.name);
-    console.log(obj.number);
-    console.log(obj.mail);
-
+  console.log(obj.id);
+  console.log(obj.name);
+  console.log(obj.number);
+  console.log(obj.mail);
 }
-//for Each
-json.forEach(function(obj) { console.log(obj.name); });
 
-//for In
-for (var key in json) {
-if (json.hasOwnProperty(key)) {
+json.forEach((obj) => {
+  console.log(obj.name);
+});
+
+for (let key in json) {
   console.log(json[key].id);
-  //console.log(json[key].name);
- 
 }
-}
-//for Of
+
 let text = "";
 for (let x of json[key].id) {
- text += x; 
+  text += x;
 }
- console.log(text);
+console.log(text);
